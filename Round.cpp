74652926120f3651a861bdffbefc8e5ced9e01fc
El3ctr0u0 <costel.lungu@student.unitbv.ro>
2023-11-03 13:59:52 +0200

    implemented endRound.

--- Round.cpp
+++ Round.cpp
@@ -1,3 +1,16 @@
 import round;
 using modern::Round;
 
+#include <thread>
+#include <chrono>
+#include<iostream>
+
+void Round::endRound()
+{
+	std::cout << "Next round start in :";
+	for (int i = 1; i < 6; i++)
+	{
+		std::cout << i << std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	}
+}
