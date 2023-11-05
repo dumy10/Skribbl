6fa8149638d1bd914c2ae874026d96b8ca6e637e
dumy10 <horoveidumitru1@gmail.com>
2023-11-05 20:25:44 +0200

    game.cpp

--- Game.cpp
+++ Game.cpp
@@ -13,14 +13,20 @@ using namespace skribbl;
 Game::Game(const std::vector<Player>& players):
 	m_players{ players }
 {
-	this->m_rounds.resize(kNoOfRounds);
-	uint8_t roundNumber{ 1 };
+
 	/*
 	Needs to generate a word for each round using random_shuffle
 	Need to end the turn and move to the next player
 	*/
+	
+	/*
+		Logic on how the rounds are initiallized needs to be reworked
+
+	this->m_rounds.resize(kNoOfRounds);
+	uint8_t roundNumber{ 1 };
 	for(Round& round : this->m_rounds)
 		round = Round(Leaderboard(this->m_players), Turn(this->m_players[0]), WordGenerator::generateWords(), roundNumber++, players);
+	*/
 }
 
 
@@ -29,8 +35,5 @@ Player Game::getWinner(const Leaderboard& leaderboard) const
 	if(leaderboard.getLeaderboard().size() == 0)
 		return Player();
 	else
-	{
-		std::set<Player> players = leaderboard.getLeaderboard();
-		return *std::max_element(players.begin(), players.end());
-	}
+		return leaderboard.getLeaderboard()[0];
 }
