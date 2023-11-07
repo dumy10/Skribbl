3ce5a29d198ab9d4543a72b32a63994aeddc2153
dumy10 <horoveidumitru1@gmail.com>
2023-11-08 00:16:27 +0200

    Added more functionality for the turn class

--- turn.cpp
+++ turn.cpp
@@ -1,17 +1,30 @@
 import turn;
 using namespace skribbl;
 
-Turn::Turn(const Player& player):
-	m_player{ player }
+Turn::Turn(const Player& player, const std::string& word):
+	m_player{ player },
+	m_word {word}
 {
 	// Empty
 }
-void Turn::setPlayerTurn(const Player& player)
+
+void Turn::setPlayerTurn(const Player& player, const std::string& word)
 {
 	this->m_player = player;
+	this->m_word = word;
 }
 
 const Player& Turn::getCurrentPlayer() const noexcept
 {
 	return this->m_player;
+}
+
+const std::string_view Turn::getCurrentWord() const noexcept
+{
+	return this->m_word;
+}
+
+const bool Turn::guessWord(const std::string& word) const
+{
+	return this->m_word == word;
 }
--- turn.cppm
+++ turn.cppm
@@ -1,6 +1,7 @@
 export module turn;
 
 import player;
+import <string_view>;
 
 
 namespace skribbl
@@ -8,11 +9,13 @@ namespace skribbl
 	export class Turn
 	{
 	public:
-		Turn(const Player& player);
-		void setPlayerTurn(const Player& player);
+		Turn(const Player& player, const std::string& word);
+		void setPlayerTurn(const Player& player, const std::string& word);
 		const Player& getCurrentPlayer() const noexcept;
-
+		const std::string_view getCurrentWord() const noexcept;
+		const bool guessWord(const std::string& word) const;
 	private:
 		Player m_player;
+		std::string m_word;
 	};
 }
