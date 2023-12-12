c6f1b5857337188aac57204356e7c8da2e83ca42
El3ctr0u0 <costel.lungu@student.unitbv.ro>
2023-12-12 15:20:09 +0200

    updated leaderboards to calculate the points of the players

--- Server/Leaderboard.cpp
+++ Server/Leaderboard.cpp
@@ -26,3 +26,50 @@ void Leaderboard::setLeaderboard(const std::vector<Player>& players)
 	std::sort(this->m_scoreboard.begin(), this->m_scoreboard.end(), std::greater<>());
 
 }
+
+void skribbl::Leaderboard::addPoints(Player& player,const int& timeLeft)
+{
+	int points = 0;
+	if (timeLeft > 30)
+	{
+		points = 100;
+		player.addPoints(points);
+	}
+	else {
+		if (timeLeft > 0)
+		{
+			points = int((60 - timeLeft) * 100 / 30);
+			player.addPoints(points);
+		}
+		else {
+			substractPoints(player);
+		}
+	}
+}
+
+void skribbl::Leaderboard::substractPoints(Player& player)
+{
+	int points = 0;
+	points = 50;
+	player.substractPoints(points);
+}
+
+void skribbl::Leaderboard::addPointsForTheDrawer(Player& player)
+{
+	int points=int((60-m_averageTime)*100/60);
+	player.addPoints(points);
+}
+
+void skribbl::Leaderboard::substractPointsForTheDrawer(Player& player)
+{
+	int points = 0;
+	points = 50;
+	player.substractPoints(points);
+}
+
+void skribbl::Leaderboard::averageTime(const int& timeLeft)
+{
+	this->m_averageTime += 60-timeLeft;
+	this->m_averageTime /= 2;
+}
+
--- Server/leaderboard.cppm
+++ Server/leaderboard.cppm
@@ -13,7 +13,14 @@ namespace skribbl
         Leaderboard(const std::vector<Player>& players);
         const std::vector<Player> getLeaderboard() const noexcept;
         void setLeaderboard(const std::vector<Player>& players);
+        void addPoints(Player& player,const int& timeLeft);
+        void substractPoints(Player& player);
+        void addPointsForTheDrawer(Player& player);
+        void substractPointsForTheDrawer(Player& player);
+        void averageTime(const int& timeLeft);
+
     private:
         std::vector<Player> m_scoreboard;
+        float m_averageTime=0;
 };
 }
